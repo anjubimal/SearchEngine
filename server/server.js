e70667c23bde08035d3e1036df6223360badf4c0
anjubimal <anjubimal@tamu.edu>
2021-08-10 15:51:55 -0400

    backend

--- server/server.js
+++ server/server.js
@@ -3,11 +3,28 @@ const path = require('path');
 const db = require('./config/connection');
 const routes = require('./routes');
 
+const { ApolloServer } = require('apollo-server-express');
+const { authMiddleware } = require('./utils/auth');
+
+
+// import our typeDefs and resolvers
+const { typeDefs, resolvers } = require('./schemas');
+
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  context: authMiddleware
+});
+
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+
+
+
 
 // if we're in production, serve client/build as static assets
 if (process.env.NODE_ENV === 'production') {
